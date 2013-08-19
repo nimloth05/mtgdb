99b07bdb8497354fa73d0ee75096e61bd0d7cab2
Sandro <morpheus.rabbithole@gmail.com>
2013-08-19 13:37:06 +0200

    - A change

---
+++ Code/Java/ui/src/main/java/org/mtgdb/launche/Main.java
@@ -0,0 +1,11 @@
+package org.mtgdb.launche;
+
+/**
+ * @author Sandro Orlando
+ */
+public final class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Jedes Programm startet mit einem Main!");
+  }
+}
