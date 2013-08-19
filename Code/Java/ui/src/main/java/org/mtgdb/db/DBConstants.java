a481c29b64f2a25428ca13f3ec271c82d2af9909
Sandro <morpheus.rabbithole@gmail.com>
2013-08-19 18:31:32 +0200

    - Datenbank eingebunden

---
+++ Code/Java/ui/src/main/java/org/mtgdb/db/DBConstants.java
@@ -0,0 +1,12 @@
+package org.mtgdb.db;
+
+/**
+ * @author Sandro Orlando
+ */
+public final class DBConstants {
+
+  private DBConstants() {}
+
+  public static final String EDITION_TABLE = "Edition";
+
+}
