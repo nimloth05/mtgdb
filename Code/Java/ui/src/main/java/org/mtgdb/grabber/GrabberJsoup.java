ca4ae3eaf1c583941e75e8fa5056506ea959f187
Michael Sacher <michael.sacher@gmail.com>
2013-08-19 17:03:47 +0200

    preliminary version of card grabber

--- Code/Java/ui/src/main/java/org/mtgdb/grabber/GrabberJsoup.java
+++ Code/Java/ui/src/main/java/org/mtgdb/grabber/GrabberJsoup.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 public final class GrabberJsoup {
 
   private final String url;
+  private final String urlPrefix = "http://magiccards.info/";
 
   public GrabberJsoup(final String url) {
     this.url = url;
@@ -23,23 +24,128 @@ public final class GrabberJsoup {
     Thread thread = new Thread(new Runnable() {
       public void run() {
         try {
-          grabberText.grab();
+          grabberText.grabEdition(url);
         } catch (IOException e) {
 
         }
       }
-    }, "grabberText");
+    }, "grabAllEditions");
     thread.start();
   }
 
-  private void grab() throws IOException {
-    Document doc = Jsoup.connect(url).get();
-    Elements tables = doc.select("table .even");
-    for (Element row : tables) {
-      final Elements td = row.select("td");
-      System.out.println("Card: " + td.get(0).text() + " " + td.get(1).text() + " url: " + td.get(1).child(0).attr("href"));
-    }
-    System.out.println(tables);
+  private void grabAllEditions() throws IOException {
+
   }
 
+  private void grabEdition(final String editionUrl) throws IOException {
+
+    Document doc = Jsoup.connect(editionUrl).get();
+    Elements tables = doc.select("table .even");
+    tables.addAll(doc.select("table .odd"));
+
+    for (Element row : tables) {
+      final Elements td = row.select("td");
+     // System.out.println("Card: " + td.get(0).text() + " " + td.get(1).text() + " url: " + td.get(1).child(0).attr("href"));
+
+      //System.out.println("\n\nurl: "+urlPrefix+td.get(1).child(0).attr("href")+"\nEdition: "+doc.title()+"\nRarity: "+td.get(4).text()+ "\nName: " +td.get(1).text());
+      grabCard(""+urlPrefix+td.get(1).child(0).attr("href"),doc.title(),td.get(4).text(),td.get(1).text());
+    }
+//    System.out.println(tables);
+  }
+
+  private void grabCard(final String cardUrl, final String edition, final String rarity, final String name) throws IOException {
+
+    // final java.util.regex.Pattern patternEditionRarity = java.util.regex.Pattern.compile("");
+
+    java.net.URL urlObject = new java.net.URL(cardUrl);
+    final java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.InputStreamReader(urlObject.openConnection().getInputStream(), "utf8"));
+    String line;
+    StringBuilder builder = new StringBuilder();
+    while ((line = bufferedReader.readLine()) != null) {
+      builder.append(line);
+    }
+
+    final String html = builder.toString();
+    org.mtgdb.model.CardDescription card = new org.mtgdb.model.CardDescription();
+
+    extractTypeLine(card, html);
+    extractTypeLineOther(card, html);
+    extractTypeLineCreature(card, html);
+    extractCardText(card, html);
+    extractFlavourText(card, html);
+    extractImageURL(card, html);
+    extractNrArtist(card, html);
+    card.setEdition(edition);
+    card.setRarity(rarity);
+    card.setName(name);
+
+    System.out.println("=========================================================");
+    System.out.println(card.toString());
+
+  }
+
+  private void extractTypeLine(org.mtgdb.model.CardDescription card, final String html) {
+    final java.util.regex.Pattern patternTypeline = java.util.regex.Pattern.compile("<p>(.*),\\s\\n\\s*([0-9A-Z]+)\\s\\(([0-9]+)\\)\\n.*</p>");
+    java.util.regex.Matcher m = patternTypeline.matcher(html);
+    if (m.matches()) {
+      card.setType(m.group(1));
+      card.setManaCost(m.group(2));
+      card.setConvManaCost(Integer.parseInt(m.group(3)));
+    }
+  }
+
+  private void extractTypeLineOther(org.mtgdb.model.CardDescription card, final String html) {
+    final java.util.regex.Pattern patternTypelineOther = java.util.regex.Pattern.compile("<p>(.*)\\s—\\s(.*)\\n.*");
+    java.util.regex.Matcher m = patternTypelineOther.matcher(html);
+    if (m.matches()) {
+      card.setType(m.group(1));
+      card.setSubType(m.group(2));
+    }
+  }
+
+  private void extractTypeLineCreature(org.mtgdb.model.CardDescription card, final String html) {
+    final java.util.regex.Pattern patternTypelineCreature = java.util.regex.Pattern.compile("<p>(.*)\\s—\\s(.*)\\s(\\d{1,2}|\\*)/(\\d{1,2}|\\*),\\s\\n\\s*([0-9A-Z]+)\\s\\(([0-9]+)\\)\\n.*</p>");
+    java.util.regex.Matcher m = patternTypelineCreature.matcher(html);
+    if (m.matches()) {
+      card.setType(m.group(1));
+      card.setSubType(m.group(2));
+      card.setPower(Integer.parseInt(m.group(3)));
+      card.setHp(Integer.parseInt(m.group(4)));
+      card.setManaCost(m.group(5));
+      card.setConvManaCost(Integer.parseInt(m.group(6)));
+    }
+  }
+
+  private void extractCardText(org.mtgdb.model.CardDescription card, final String html) {
+    final java.util.regex.Pattern patternCardText = java.util.regex.Pattern.compile("<p class=\"ctext\"><b>(.*)</b></p>");
+    java.util.regex.Matcher m = patternCardText.matcher(html);
+    if (m.matches()) {
+      card.setCardText(m.group(1));
+    }
+  }
+
+  private void extractFlavourText(org.mtgdb.model.CardDescription card, final String html) {
+    final java.util.regex.Pattern patternFlavorText = java.util.regex.Pattern.compile("<p><i>(.*)</i></p>");
+    java.util.regex.Matcher m = patternFlavorText.matcher(html);
+    if (m.matches()) {
+      card.setFlavorText(m.group(1));
+    }
+  }
+
+  private void extractImageURL(org.mtgdb.model.CardDescription card, final String html) {
+    final java.util.regex.Pattern patternScanUrl = java.util.regex.Pattern.compile("\"(http://magiccards\\.info/scan.*)\"");
+    java.util.regex.Matcher m = patternScanUrl.matcher(html);
+    if (m.matches()) {
+      card.setImageURL(m.group(1));
+    }
+  }
+
+  private void extractNrArtist(org.mtgdb.model.CardDescription card, final String html) {
+    final java.util.regex.Pattern patternCardnumArtist = java.util.regex.Pattern.compile("#(\\d{1,3})\\s\\((.*)\\)");
+    java.util.regex.Matcher m = patternCardnumArtist.matcher(html);
+    if (m.matches()) {
+      card.setNumber(m.group(1));
+      card.setArtist(m.group(2));
+    }
+  }
 }
--- Code/Java/ui/src/main/java/org/mtgdb/model/CardDescription.java
+++ Code/Java/ui/src/main/java/org/mtgdb/model/CardDescription.java
@@ -4,5 +4,150 @@ package org.mtgdb.model;
  * @author Sandro Orlando
  */
 public final class CardDescription {
+  private String type;
+  private String subType;
+  private String manaCost;
+  private int convManaCost;
+  private int power;
+  private int hp;
+  private String imageURL;
+  private String cardText;
+  private String flavorText;
+  private String number;
+  private String artist;
+  private String edition;
+  private String rarity;
+  private String name;
 
+  public String getImageURL() {
+    return imageURL;
+  }
+
+  public void setImageURL(String imageURL) {
+    this.imageURL = imageURL;
+  }
+
+  public int getHp() {
+    return hp;
+  }
+
+  public void setHp(int hp) {
+    this.hp = hp;
+  }
+
+  public int getPower() {
+    return power;
+  }
+
+  public void setPower(int power) {
+    this.power = power;
+  }
+
+  public int getConvManaCost() {
+    return convManaCost;
+  }
+
+  public void setConvManaCost(int convManaCost) {
+    this.convManaCost = convManaCost;
+  }
+
+  public String getManaCost() {
+    return manaCost;
+  }
+
+  public void setManaCost(String manaCost) {
+    this.manaCost = manaCost;
+  }
+
+  public String getSubType() {
+    return subType;
+  }
+
+  public void setSubType(String subType) {
+    this.subType = subType;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getCardText() {
+    return cardText;
+  }
+
+  public void setCardText(String cardText) {
+    this.cardText = cardText;
+  }
+
+  public String getFlavorText() {
+    return flavorText;
+  }
+
+  public void setFlavorText(String flavorText) {
+    this.flavorText = flavorText;
+  }
+
+  public void setNumber(String number) {
+    this.number = number;
+  }
+
+  public String getNumber() {
+    return number;
+  }
+
+  public void setArtist(String artist) {
+    this.artist = artist;
+  }
+
+  public String getArtist() {
+    return artist;
+  }
+
+  public void setEdition(String edition) {
+    this.edition = edition;
+  }
+
+  public String getEdition() {
+    return edition;
+  }
+
+  public void setRarity(String rarity) {
+    this.rarity = rarity;
+  }
+
+  public String getRarity() {
+    return rarity;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public String toString() {
+    return "CardDescription{" +
+      "artist='" + artist + '\'' +
+      ", type='" + type + '\'' +
+      ", subType='" + subType + '\'' +
+      ", manaCost='" + manaCost + '\'' +
+      ", convManaCost=" + convManaCost +
+      ", power=" + power +
+      ", hp=" + hp +
+      ", imageURL='" + imageURL + '\'' +
+      ", cardText='" + cardText + '\'' +
+      ", flavorText='" + flavorText + '\'' +
+      ", number='" + number + '\'' +
+      ", edition='" + edition + '\'' +
+      ", rarity='" + rarity + '\'' +
+      ", name='" + name + '\'' +
+      '}';
+  }
 }
