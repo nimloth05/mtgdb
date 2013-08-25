20ab6966dd40c1374dd913dd5305b6e03880574f
Michael Sacher <michael.sacher@gmail.com>
2013-08-25 08:55:04 +0200

    added a static image of the card

--- Code/Java/ui/src/main/java/org/mtgdb/ui/MainWindow.java
+++ Code/Java/ui/src/main/java/org/mtgdb/ui/MainWindow.java
@@ -3,10 +3,13 @@ package org.mtgdb.ui;
 import net.miginfocom.swing.MigLayout;
 import org.mtgdb.ui.util.frame.FrameFactory;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * @author Sandro Orlando
@@ -29,13 +32,26 @@ public final class MainWindow {
   private void createContentArea() {
     JPanel panel = new JPanel();
     panel.setLayout(new MigLayout());
-    panel.add(new JLabel("Your Library:"),"wrap");
+    panel.add(new JLabel("Your Library:"), "wrap");
     final JTable table = new JTable();
     final JScrollPane pane = new JScrollPane(table);
     table.setModel(model.getLibraryModel());
-    panel.add(table.getTableHeader(),"wrap");
-    panel.add(pane,"width :100%:");
+    final JLabel imgLbl = createImageLabel("http://magiccards.info/scans/en/pr/133.jpg");
+
+    panel.add(imgLbl, "spany 2");
+    panel.add(table.getTableHeader(), "wrap");
+    panel.add(pane, "width :100%:");
     frame.getContentPane().add(panel, BorderLayout.CENTER);
+  }
+
+  private JLabel createImageLabel(String urlString) {
+    Image scan = null;
+    try {
+      URL url = new URL(urlString);
+      scan = ImageIO.read(url);
+    } catch (IOException e) {
+    }
+    return new JLabel(new ImageIcon(scan));
   }
 
   private void createToolbar() {
@@ -47,7 +63,7 @@ public final class MainWindow {
   }
 
   public void show() {
-    frame = FrameFactory.createCenteredFrame("MTG Administration", 640, 480);
+    frame = FrameFactory.createCenteredFrame("MTG Administration", 800, 600);
     FrameFactory.setMainFrame(frame);
     frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     frame.addWindowListener(new WindowAdapter() {
